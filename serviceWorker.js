ba685a7d886ed1b583f103ac05d2558350059427
istuffer-malpack <60739047+istuffer-malpack@users.noreply.github.com>
2021-05-30 09:34:26 -0400

    Add files via upload

---
+++ serviceWorker.js
@@ -0,0 +1,21 @@
+const cacheName = "covid-19-screening-app";
+const assets = [
+  "/",
+  "/index.html"
+];
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
